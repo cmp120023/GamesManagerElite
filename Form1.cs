d328f39b000fb1c9af50b345bd6312f2cd878ad1
cody pekarek <codym.pekarek@hotmail.com>
2026-07-29 13:21:43 -0700

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,12 @@
+namespace GamesManagerElite
+{
+    public partial class Form_GameManagerElite : Form
+    {
+        public Form_GameManagerElite()
+        {
+            InitializeComponent();
+        }
+
+
+    }
+}
