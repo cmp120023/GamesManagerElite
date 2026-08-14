3c316727afaeb705da6f80de4b0ad2925f0bfde2
cody pekarek <codym.pekarek@hotmail.com>
2026-08-14 11:57:53 -0700

    Reset all files to be an MVC app.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using Game_Manager_Elite.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Game_Manager_Elite.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
