e331104b51528aa3b48a96ad05323433a6e4bb13
cody pekarek <codym.pekarek@hotmail.com>
2026-08-14 20:25:44 -0700

    Started the initial structure of the home page. logos were added, labels, textboxes and a basic CSS structure for design.

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,34 @@
+﻿using GamesManagerElite.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GamesManagerElite.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpPost]
+        [ValidateAntiForgeryToken] 
+        public IActionResult Login(LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            bool isValidUser = true;
+
+            if (isValidUser)
+            {
+                if (model.LeagueType == "APA")
+                {
+                    return RedirectToAction("Index", "Apa");
+                }
+                else if (model.LeagueType == "BCA")
+                {
+                    return RedirectToAction("Index", "Bca");
+                }
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ Models/LoginViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GamesManagerElite.Models
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Please select a league.")]
+        public string LeagueType { get; set; } = "APA";
+
+        [Required(ErrorMessage = "Username or email is required.")]
+        public string Username { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Password is required.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = string.Empty;
+    }
+}
