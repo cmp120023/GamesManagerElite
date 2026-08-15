794423136789a35dd836bd01e626868a20ef4f30
France A <133005536+fafafafrance@users.noreply.github.com>
2026-08-15 05:45:38 -0700

    docs(bca): add XML documentation summaries to controller actions, models, and views

--- Program.cs
+++ Program.cs
@@ -1,6 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using Game_Manager_Elite.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<LeagueContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
